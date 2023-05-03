7cbdb9080c76afb96765b739e4fd939a3ca4e6e1
scamer300 <scamer300@caledonian.ac.uk>
2023-05-03 19:00:16 +0100

    adding register user functionality

--- controllers/healthifyControllers.js
+++ controllers/healthifyControllers.js
@@ -1,3 +1,5 @@
+const userDao = require('../models/userModel.js');
+
 exports.show_landing_page = function(req, res) {
     res.render("landing");
 }
@@ -24,4 +26,25 @@ exports.show_goals_page = function (req, res) {
 
 exports.show_complete_page = function (req, res) {
     res.render("complete");
+}
+
+//Register New User
+exports.post_new_user = function(req, res) {
+    const name = req.body.fname;
+    const user = req.body.username;
+    const password = req.body.pass;
+
+    if (!name || !user || !password) {
+        res.send(401, 'no user or no password');
+        return;
+    }
+    userDao.lookup(user, function(err, u) {
+    if (u) {
+        res.send(401, "User exists:", user);
+        return;
+    }
+    userDao.create(name, user, password);
+    console.log("register user", user, "password", password);
+    res.redirect('/login');
+    });
 }
--- routes/healthifyRoutes.js
+++ routes/healthifyRoutes.js
@@ -5,9 +5,12 @@ const controller = require('../controllers/healthifyControllers');
 router.get('/', controller.show_landing_page);
 router.get('/home', controller.show_landing_page);
 router.get('/about', controller.show_about_page);
-router.get('/register', controller.show_register_page);
-router.get('/login', controller.show_login_page);
 router.get('/info', controller.show_info_page);
+
+router.get('/register', controller.show_register_page);
+router.post('/register', controller.post_new_user);
+router.get('/login', controller.show_login_page);
+
 router.get('/goals', controller.show_goals_page);
 router.get('/complete', controller.show_complete_page);
 
@@ -15,6 +18,12 @@ router.use(function(req, res) {
     res.status(404);
     res.type('text/plain');
     res.send('404 Not found.');
+})
+
+router.use(function(err, req, res, next) {
+    res.status(500);
+    res.type('text/plain');
+    res.send('Internal Server Error.');
 })
 
 module.exports = router;
